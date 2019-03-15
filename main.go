6e38256a5d872d9876dc9aa87c3a728ba44d125a
liangzuobin <liangzuobin@meican.com>
2019-03-15 09:15:07 +0800

    Feat: second

--- main.go
+++ main.go
@@ -3,42 +3,69 @@ package main
 import (
 	"fmt"
 	"os"
-
-	"github.com/spf13/cobra"
+	"os/exec"
+	"strings"
 )
 
-var msg string
-
 func main() {
-	var cmd = &cobra.Command{
-		Use:     "git ng",
-		Short:   "git commit with angular style message",
-		Long:    `git commit with angular style message`,
-		Example: `  git ng -f 'new feature'`,
-		Args:    cobra.ExactArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			if err := run(); err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-		},
-	}
+	args := os.Args[1:]
 
-	cmd.Flags().StringVarP(&msg, "feat", "f", "", "feat")
-	cmd.Flags().StringVarP(&msg, "fix", "x", "", "fix")
-	cmd.Flags().StringVarP(&msg, "docs", "d", "", "docs")
-	cmd.Flags().StringVarP(&msg, "style", "s", "", "style")
-	cmd.Flags().StringVarP(&msg, "refactor", "r", "", "refactor")
-	cmd.Flags().StringVarP(&msg, "perf", "p", "", "perf")
-	cmd.Flags().StringVarP(&msg, "test", "t", "", "refactor")
-	cmd.Flags().StringVarP(&msg, "chore", "c", "", "chore")
-
-	if err := cmd.Execute(); err != nil {
-		fmt.Println(err)
+	if len(args) != 2 {
+		printhelp()
 		os.Exit(1)
 	}
+
+	name, val := args[0], args[1]
+	if strings.Contains(name, "-") {
+		name = strings.Replace(name, "-", "", -1)
+	}
+
+	var prefix string
+	switch name {
+	case "f", "feat":
+		prefix = "Feat: "
+	case "x", "fix":
+		prefix = "Fix: "
+	case "d", "docs":
+		prefix = "Docs: "
+	case "s", "style":
+		prefix = "Style: "
+	case "r", "refactor":
+		prefix = "Refactor: "
+	case "p", "perf":
+		prefix = "Perf: "
+	case "t", "test":
+		prefix = "Test: "
+	case "c", "chore":
+		prefix = "Chore: "
+	default:
+		printhelp()
+		os.Exit(1)
+	}
+
+	if err := exec.Command("git", "commit", "-m", prefix+val).Run(); err != nil {
+		panic(err)
+	}
+
+	os.Exit(0)
 }
 
-func run() error {
-	return nil
+func printhelp() {
+	str := `Useage:
+  git ng -flag message.
+Example:
+  git ng -f new feat.
+Flags:
+  must be one (and the only one) of the following:
+  -f --feat: A new feature
+  -x --fix: A bug fix
+  -d --docs: Documentation only changes
+  -s --style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
+  -r --refactor: A code change that neither fixes a bug or adds a feature
+  -p --perf: A code change that improves performance
+  -t --test: Adding missing tests
+  -c --chore: Changes to the build process or auxiliary tools and libraries such as documentation generation
+
+If you see the source code, you'll know I'm teasing you.`
+	fmt.Println(str)
 }
